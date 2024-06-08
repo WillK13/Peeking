ef78be8bc5d238f2dec0290f62f969ed53fc1e28
WillK13 <30449185+WillK13@users.noreply.github.com>
2024-06-08 13:44:53 -0400

    Fixing up the tier pages

--- Peeking/Main Pages/MainView.swift
+++ Peeking/Main Pages/MainView.swift
@@ -9,102 +9,105 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        ZStack {
-            BackgroundView()
-            VStack {
-                //Top Area
-                HStack {
-                    
+        
+        NavigationView {
+            ZStack {
+                BackgroundView()
+                VStack {
+                    //Top Area
+                    HStack {
+                        
                         HStack(spacing: 10.0) {
                             Image(systemName: "heart.fill").foregroundColor(.red).padding(/*@START_MENU_TOKEN@*/EdgeInsets()/*@END_MENU_TOKEN@*/).padding([.top, .leading, .bottom]).font(.system(size: 35))
                             Text("3").font(.title).padding(.trailing)
                         }.background(RoundedRectangle(cornerRadius: 8)   .foregroundColor(.white)     ).padding([.top, .leading], 20.0)
                         
-                    
-                    Spacer()
-                    Spacer()
-                    
-                    Image("Duck_Head").resizable().aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fit/*@END_MENU_TOKEN@*/).frame(width: 120).padding(.top, 10.0)
-                    
-                    
-                    Spacer()
-                    
-                    
-                    
-                    VStack {
-                        Text("Tiers").font(.largeTitle).padding(.top, 5.0)
-                        Image(systemName: "bag").font(.system(size: 45)).padding(/*@START_MENU_TOKEN@*/.horizontal, 27.0/*@END_MENU_TOKEN@*/).padding(.bottom, 10.0)
-                    } .background(RoundedRectangle(cornerRadius: 8)   .foregroundColor(.white)     )
-                }.padding([.top, .trailing], 20.0)
-                
-                //Main Area
-                ZStack {
-                    Rectangle()
-                        .fill(Color.white)
-                        .frame(height: 525)
-                        .cornerRadius(10)
-                    
-                    VStack(alignment: .trailing) {
                         
+                        Spacer()
+                        Spacer()
+                        
+                        Image("Duck_Head").resizable().aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fit/*@END_MENU_TOKEN@*/).frame(width: 120).padding(.top, 10.0)
+                        
+                        
+                        Spacer()
+                        
+                        
+                        NavigationLink(destination: EmployeeTierView()) {
+                            VStack {
+                                Text("Tiers").font(.largeTitle).padding(.top, 5.0)
+                                Image(systemName: "bag").font(.system(size: 45)).padding(/*@START_MENU_TOKEN@*/.horizontal, 27.0/*@END_MENU_TOKEN@*/).padding(.bottom, 10.0)
+                            } .background(RoundedRectangle(cornerRadius: 8)   .foregroundColor(.white)     ) }
+                    }.padding([.top, .trailing], 20.0)
+                    
+                    //Main Area
+                    ZStack {
+                        Rectangle()
+                            .fill(Color.white)
+                            .frame(height: 525)
+                            .cornerRadius(10)
+                        
+                        VStack(alignment: .trailing) {
+                            
                             Image(systemName: "bookmark")
                                 .resizable()
                                 .frame(width: 40, height: 50)
                                 .foregroundColor(.black).padding(.top, 10)
-                        
-                        Spacer()
-                       
+                            
+                            Spacer()
+                            
                             Image(systemName: "heart")
                                 .resizable()
                                 .frame(width: 40, height: 40)
                                 .padding(.bottom, 10).foregroundColor(.black)
-                        
-                       
+                            
+                            
                             Image(systemName: "ellipsis").resizable()
                                 .frame(width: 40, height: 9)
                                 .padding(.bottom, 10).foregroundColor(.black)
-                        
-                        
-                        
-                        
-                        HStack() {
-                            RoundedRectangle(cornerRadius: 10)
-                                .fill(Color("SelectColor"))
-                                .frame(width: 83, height: 20).overlay(
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .stroke(Color.black, lineWidth: 2)
-                                )
-                            Spacer()
-                            RoundedRectangle(cornerRadius: 10)
-                                .fill(Color("NotSelectedColor"))
-                                .frame(width: 83, height: 20)
-                            Spacer()
-                            RoundedRectangle(cornerRadius: 10)
-                                .fill(Color("NotSelectedColor"))
-                                .frame(width: 83, height: 20)
-                            Spacer()
-                            RoundedRectangle(cornerRadius: 10)
-                                .fill(Color("NotSelectedColor"))
-                                .frame(width: 83, height: 20)
-                            Spacer()
+                            
+                            
+                            
+                            
+                            HStack() {
+                                RoundedRectangle(cornerRadius: 10)
+                                    .fill(Color("SelectColor"))
+                                    .frame(width: 83, height: 20).overlay(
+                                        RoundedRectangle(cornerRadius: 10)
+                                            .stroke(Color.black, lineWidth: 2)
+                                    )
+                                Spacer()
+                                RoundedRectangle(cornerRadius: 10)
+                                    .fill(Color("NotSelectedColor"))
+                                    .frame(width: 83, height: 20)
+                                Spacer()
+                                RoundedRectangle(cornerRadius: 10)
+                                    .fill(Color("NotSelectedColor"))
+                                    .frame(width: 83, height: 20)
+                                Spacer()
+                                RoundedRectangle(cornerRadius: 10)
+                                    .fill(Color("NotSelectedColor"))
+                                    .frame(width: 83, height: 20)
+                                Spacer()
+                            }
+                            
+                            
+                            
                         }
-                        
-                        
-                        
-                    }
-                    .frame(width: 350, height: 500)
-                }.padding(/*@START_MENU_TOKEN@*/EdgeInsets()/*@END_MENU_TOKEN@*/).padding(.bottom, 15.0)
-                
-                //Next Profile
-                ZStack(alignment: .topTrailing) {
-                    Rectangle()
-                        .fill(Color.white)
-                        .frame(height: 20)
+                        .frame(width: 350, height: 500)
+                    }.padding(/*@START_MENU_TOKEN@*/EdgeInsets()/*@END_MENU_TOKEN@*/).padding(.bottom, 15.0)
                     
-                }.padding(.bottom, 10)
-                
-            }}
-        
-    }
+                    //Next Profile
+                    ZStack(alignment: .topTrailing) {
+                        Rectangle()
+                            .fill(Color.white)
+                            .frame(height: 20)
+                        
+                    }.padding(.bottom, 10)
+                    
+                }}
+            .navigationBarHidden(true)
+        }
+        }
 }
 
 #Preview {
--- Peeking/Tiers/EmployeeTierView.swift
+++ Peeking/Tiers/EmployeeTierView.swift
@@ -9,44 +9,199 @@ import SwiftUI
 
 struct EmployeeTierView: View {
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    @State private var selectedPage = 0
+    @State private var selectedPlan = "with Floater"
+    
+    private let pages = [
+        PlanPage(title: "3 Like Capacity", description: "Refilled every 24 hours", imageName: "heart", iconText: "3"),
+        PlanPage(title: "Bookmark", description: "One bookmark for your search", imageName: "bookmark", iconText: "1X")
+    ]
+    
+    private let plans = ["with Floater", "Plan 2", "Plan 3"]
     
     var body: some View {
         ZStack {
             BackgroundView()
-            VStack{
-                Image("Duck_Head").resizable().aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fit/*@END_MENU_TOKEN@*/).frame(width: 120).padding(.top: )
+            VStack {
+                Image("Duck_Head").resizable().aspectRatio(contentMode: .fit).frame(width: 120).padding(.top, 30)
                 Spacer()
             }
-            Color.black.opacity(0.65)
+            Color.black.opacity(0.8)
                 .edgesIgnoringSafeArea(.all)
             
-            
-            
+            RoundedRectangle(cornerRadius: 10)
+                .fill(Color.white)
+                .frame(width: 373, height: 650)
+                .padding(.top, 140)
             
             
             VStack {
                 Spacer()
-                Text("Hello, World!")
-                    .font(.largeTitle)
+                Spacer()
+                Spacer()
+                Spacer()
+                Text("Billing disclaimer")
+                    .font(.title2)
+                    .foregroundColor(Color.white)
+                
+                Text("Upon purchase payment will be charged to your Apple account, and your subscription will automatically renew until you cancel in settings. Upon purchase, you agree to our Terms of Agreement and Privacy Policy.")
+                    .font(.footnote)
+                    .fontWeight(.light)
+                    .foregroundColor(Color("UnimportantText"))
+                    .multilineTextAlignment(.center)
+                    .frame(width: 300.0)
                 
                 Spacer()
                 
-                Button(action: {
-                    presentationMode.wrappedValue.dismiss()
-                }) {
-                    Text("Back")
-                        .font(.title)
-                        .padding()
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
+                VStack {
+                    Text("Just the Basics")
+                        .font(.largeTitle)
+                        .padding(.top)
+                    
+                    Picker("Plan", selection: $selectedPlan) {
+                        ForEach(plans, id: \.self) { plan in
+                            Text(plan)
+                        }
+                    }
+                    .pickerStyle(MenuPickerStyle())
+                    .background(RoundedRectangle(cornerRadius: 8).fill(Color.gray.opacity(0.2)))
+                    
+                    HStack {
+                        Button(action: {
+                            if selectedPage > 0 {
+                                selectedPage -= 1
+                            }
+                        }) {
+                            Image(systemName: "chevron.left")
+                                .font(.largeTitle)
+                        }
+                        .padding(.leading)
+                        
+                        Spacer()
+                        
+                        VStack {
+                            Image(systemName: pages[selectedPage].imageName)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 60, height: 60)
+                            
+                            Text(pages[selectedPage].iconText)
+                                .font(.title)
+                                .padding(.bottom, 2)
+                            
+                            Text(pages[selectedPage].title)
+                                .font(.title2)
+                            
+                            Text(pages[selectedPage].description)
+                                .font(.subheadline)
+                                .foregroundColor(.gray)
+                            
+                            HStack {
+                                Circle()
+                                    .fill(selectedPage == 0 ? Color.black : Color.gray)
+                                    .frame(width: 10, height: 10)
+                                
+                                Circle()
+                                    .fill(selectedPage == 1 ? Color.black : Color.gray)
+                                    .frame(width: 10, height: 10)
+                            }
+                            .padding(.top)
+                        }.padding(.top)
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            if selectedPage < pages.count - 1 {
+                                selectedPage += 1
+                            }
+                        }) {
+                            Image(systemName: "chevron.right")
+                                .font(.largeTitle)
+                        }
+                        .padding(.trailing)
+                    }
+                    
+                    Spacer()
+                    
+                    
+                    
+                    
+                    Spacer()
+                    Spacer()
+                    
+                    VStack(spacing: 0) {
+                                           VStack {
+                                               Image("Infinity").resizable().aspectRatio(contentMode: .fill).frame(width: 50)
+                                               
+                                               Text("Months")
+                                                   .font(.title2)
+                                                   .padding(.top, -20.0)
+                                           }
+                                           .frame(height: 80)
+                                           .background(Color.white)
+                                           
+                                           VStack {
+                                               Text("Free")
+                                                   .font(.title)
+                                                   .padding(.top, 2)
+                                                   .padding(.bottom, 20)
+                                           }
+                                           .frame(width: 100.0, height: 60)
+                                           .background(Color.gray.opacity(0.2))
+                                           .overlay(
+                                               Rectangle()
+                                                   .frame(height: 1)
+                                                   .foregroundColor(Color.black),
+                                               alignment: .top
+                                           )
+                                           .padding(.top, 10)
+                                       }
+                                       .frame(width: 100, height: 150)
+                                       .overlay(
+                                           RoundedRectangle(cornerRadius: 8)
+                                               .stroke(Color.black, lineWidth: 1)
+                                       )
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    Spacer()
+                    
+                    
+                        Text("All users get full access")
+                            .font(.title2)
+                            .foregroundColor(Color.black)
+                            .padding()
+                            .background(RoundedRectangle(cornerRadius: 100).fill(Color("FadedTopOrange")))
+
+                    .padding(.top, 10)
+                    
+                    Button(action: {
+                        presentationMode.wrappedValue.dismiss()
+                    }) {
+                        Text("Exit")
+                            .font(.title2)
+                            .padding()
+                            .foregroundColor(Color("UnimportantText"))
+                    }
+                    .padding(.bottom, 20)
                 }
-                .padding(.bottom, 20)
             }
         }
-        
         .navigationBarBackButtonHidden(true) // Hide the default back button
     }
+}
+
+struct PlanPage {
+    let title: String
+    let description: String
+    let imageName: String
+    let iconText: String
 }
 
 #Preview {
