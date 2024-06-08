882633e35ad12544b7ff6243cc1adf3d9073339c
WillK13 <30449185+WillK13@users.noreply.github.com>
2024-06-08 13:43:00 -0400

    Made some changes to how the tiers will open and started working on the actual tiers pages.

--- Peeking/Tiers/EmployeeTierView.swift
+++ Peeking/Tiers/EmployeeTierView.swift
@@ -8,8 +8,44 @@
 import SwiftUI
 
 struct EmployeeTierView: View {
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            BackgroundView()
+            VStack{
+                Image("Duck_Head").resizable().aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fit/*@END_MENU_TOKEN@*/).frame(width: 120).padding(.top: )
+                Spacer()
+            }
+            Color.black.opacity(0.65)
+                .edgesIgnoringSafeArea(.all)
+            
+            
+            
+            
+            
+            VStack {
+                Spacer()
+                Text("Hello, World!")
+                    .font(.largeTitle)
+                
+                Spacer()
+                
+                Button(action: {
+                    presentationMode.wrappedValue.dismiss()
+                }) {
+                    Text("Back")
+                        .font(.title)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                .padding(.bottom, 20)
+            }
+        }
+        
+        .navigationBarBackButtonHidden(true) // Hide the default back button
     }
 }
 
