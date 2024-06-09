4186a69138548b663880de60e7ae13e1fd5ed571
WillK13 <30449185+WillK13@users.noreply.github.com>
2024-06-09 17:23:03 -0400

    Changes to toolbar, opening tiers, created profile and am trying to connect it to settings.

---
+++ Peeking/Main Pages/Profile/TipsView.swift
@@ -0,0 +1,18 @@
+//
+//  TipsView.swift
+//  Peeking
+//
+//  Created by Will kaminski on 6/9/24.
+//
+
+import SwiftUI
+
+struct TipsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TipsView()
+}
