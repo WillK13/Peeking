99d8334d5e122890069fc045ee910c2d81f0ce2f
WillK13 <30449185+WillK13@users.noreply.github.com>
2024-07-20 20:09:37 -0400

    Started profile creation for employers.

---
+++ Peeking/Main Pages/Swiping/ProfileCardViewEmployer.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileCardViewEmployer.swift
+//  Peeking
+//
+//  Created by Will kaminski on 7/20/24.
+//
+
+import SwiftUI
+
+struct ProfileCardViewEmployer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileCardViewEmployer()
+}
