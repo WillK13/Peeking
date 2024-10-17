0fb4b97528f63b817f6c4ee8b67a4e3d99c84df3
WillK13 <30449185+WillK13@users.noreply.github.com>
2024-10-16 22:08:37 -0400

    Bookmark fixes

--- Peeking/Main Pages/BookmarkView.swift
+++ Peeking/Main Pages/BookmarkView.swift
@@ -40,40 +40,166 @@ struct BookmarkView: View {
                         Spacer()
                     }
                     
-                    Text("Bookmark:")
+                    Text("Bookmarks")
                         .font(.title)
-                        .foregroundColor(.white)
-                    
-                    NavigationLink(destination: ProfileShare(userId: .constant(userId), needsButtons: .constant(true)).navigationBarBackButtonHidden().toolbar(.hidden, for: .tabBar)) {
-                        AsyncImage(url: url) { image in
-                            image
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 100, height: 150)
-                        } placeholder: {
-                            ProgressView()
-                                .frame(width: 100, height: 100)
-                                .background(Color.white)
-                        }
-                    }.padding(20)
-                    .cornerRadius(5)
-                }
-                .background(Color.gray)
-                .cornerRadius(25)
-                .shadow(radius: 10)
-                .padding(.horizontal, 50)
-                .padding(.vertical, 15)
-            } else {
-                Text("No Bookmark Found")
-                    .foregroundColor(.white)
-                    .font(.title)
-                    .padding()
-                    .background(Color.gray)
-                    .cornerRadius(15)
-                    .shadow(radius: 10)
-                    .padding()
-            }
-        }
+                        .foregroundColor(.black)
+                    Text("24 hour holding limit")
+                        .font(.headline)
+                        .foregroundColor(.black)
+                        .italic()
+                    HStack(spacing: 20) {
+                                            ZStack(alignment: .topTrailing) {
+                                                NavigationLink(destination: ProfileShare(userId: .constant(userId), needsButtons: .constant(true))
+                                                    .navigationBarBackButtonHidden()
+                                                    .toolbar(.hidden, for: .tabBar)) {
+                                                        AsyncImage(url: url) { image in
+                                                            image
+                                                                .resizable()
+                                                                .aspectRatio(contentMode: .fit)
+                                                                .cornerRadius(5)
+                                                                .frame(width: 100, height: 175)
+                                                                .padding(.bottom, 15)
+                                                                .padding(.bottom, 25)
+                                                        } placeholder: {
+                                                            ProgressView()
+                                                                .frame(width: 100, height: 100)
+                                                                .background(Color.white)
+                                                        }
+                                                }
+                                                .padding([.top, .leading, .bottom], 20)
+                                                
+                                                // Red circle with minus button
+                                                Button(action: {
+                                                    removeBookmark()
+                                                }) {
+                                                    Image(systemName: "minus.circle.fill")
+                                                        .foregroundColor(.red)
+                                                        .font(.system(size: 20))
+                                                        .background(Color.white)
+                                                        .clipShape(Circle())
+                                                        .padding(.top, 20)
+                                                        .padding(.leading, 15)
+                                                }
+                                                /*.offset(x: 10, y: 10)*/ // Position at the top-right of the image
+                                            }
+                                            
+                                            VStack {
+                                                Image(systemName: "lock")
+                                                    .padding(.vertical, 25)
+                                                    .padding(.horizontal, 10)
+                                                    .font(.system(size: 75))
+                                                    .foregroundColor(Color.black)
+                                                    .background(Color.gray)
+                                                    .opacity(0.3)
+                                                    .cornerRadius(5)
+                                                Text("Get Glider")
+                                                    .font(.caption)
+                                                    .padding(.horizontal, 10)
+                                                    .padding(.vertical, 5)
+                                                    .background(Color.blue.opacity(0.2))
+                                                    .cornerRadius(20)
+                                                    .padding(.top, 5)
+                                            }
+                                            VStack {
+                                                Image(systemName: "lock")
+                                                    .padding(.vertical, 25)
+                                                    .padding(.horizontal, 10)
+                                                    .font(.system(size: 75))
+                                                    .foregroundColor(Color.black)
+                                                    .background(Color.gray)
+                                                    .opacity(0.3)
+                                                    .cornerRadius(5)
+                                                Text("Get Diver")
+                                                    .font(.caption)
+                                                    .padding(.horizontal, 10)
+                                                    .padding(.vertical, 5)
+                                                    .background(Color.purple.opacity(0.2))
+                                                    .cornerRadius(20)
+                                                    .padding(.top, 5)
+                                            }.padding(.trailing, 20)
+                                        }
+                                    }
+                                    .background(Color.white)
+                                    .cornerRadius(25)
+                                    .shadow(radius: 10)
+                                    .padding(.horizontal, 50)
+                                    .padding(.vertical, 15)
+                                } else {
+                                    VStack(spacing: 10)
+                                    {
+                                        HStack {
+                                            Button(action: {
+                                                showBookmarkView = false // Close the popup
+                                            }) {
+                                                Text("X")
+                                                    .font(.title3)
+                                                    .fontWeight(.bold)
+                                                    .foregroundColor(.white)
+                                                    .padding([.top, .leading, .trailing])
+                                            }
+                                            Spacer()
+                                        }
+                                        
+                                        Text("Bookmarks")
+                                            .font(.title)
+                                            .foregroundColor(.black)
+                                        Text("24 hour holding limit")
+                                            .font(.headline)
+                                            .foregroundColor(.black)
+                                            .italic()
+                                        HStack(spacing: 20) {
+                                                Image(systemName: "lock.open")
+                                                    .padding(.vertical, 35)
+                                                    .padding(.horizontal, 5)
+                                                    .font(.system(size: 60))
+                                                    .foregroundColor(Color.black)
+                                                    .background(Color.gray)
+                                                    .opacity(0.3)
+                                                    .cornerRadius(5)
+                                                    .padding(.bottom, 20)
+                                            VStack {
+                                                Image(systemName: "lock")
+                                                    .padding(.vertical, 25)
+                                                    .padding(.horizontal, 10)
+                                                    .font(.system(size: 75))
+                                                    .foregroundColor(Color.black)
+                                                    .background(Color.gray)
+                                                    .opacity(0.3)
+                                                    .cornerRadius(5)
+                                                Text("Get Glider")
+                                                    .font(.caption)
+                                                    .padding(.horizontal, 10)
+                                                    .padding(.vertical, 5)
+                                                    .background(Color.blue.opacity(0.2))
+                                                    .cornerRadius(20)
+                                                    .padding(.top, 5)
+                                            }
+                                            VStack {
+                                                Image(systemName: "lock")
+                                                    .padding(.vertical, 25)
+                                                    .padding(.horizontal, 10)
+                                                    .font(.system(size: 75))
+                                                    .foregroundColor(Color.black)
+                                                    .background(Color.gray)
+                                                    .opacity(0.3)
+                                                    .cornerRadius(5)
+                                                Text("Get Diver")
+                                                    .font(.caption)
+                                                    .padding(.horizontal, 10)
+                                                    .padding(.vertical, 5)
+                                                    .background(Color.purple.opacity(0.2))
+                                                    .cornerRadius(20)
+                                                    .padding(.top, 5)
+                                            }.padding(.trailing, 20)
+                                        }.padding(.bottom, 20)
+                                        }
+                                        .background(Color.white)
+                                        .cornerRadius(15)
+                                        .shadow(radius: 10)
+                                        .padding()
+                                    
+                                }
+                            }
         .onAppear {
             fetchBookmarkPhoto()
         }
@@ -123,6 +249,37 @@ struct BookmarkView: View {
             }
         }
     }
+    private func removeBookmark() {
+            guard let currentUserId = Auth.auth().currentUser?.uid else { return }
+            let db = Firestore.firestore()
+            
+            let userDocRef = db.collection("users").document(currentUserId)
+            
+            userDocRef.getDocument { document, error in
+                if let document = document, document.exists {
+                    if let userType = document.data()?["user_type"] as? Int {
+                        let bookmarkSourceDocRef: DocumentReference
+                        
+                        // Determine whether to look in the main document or the profile subcollection
+                        if userType == 1 {
+                            bookmarkSourceDocRef = db.collection("users").document(currentUserId).collection("profile").document("profile_data")
+                        } else {
+                            bookmarkSourceDocRef = userDocRef
+                        }
+                        
+                        bookmarkSourceDocRef.updateData(["bookmarks": FieldValue.arrayRemove([bookmarkedUserId!])]) { error in
+                            if let error = error {
+                                print("Error removing bookmark: \(error.localizedDescription)")
+                            } else {
+                                print("Bookmark removed successfully")
+                                self.bookmarkedUserId = nil
+                                self.photoURL = nil
+                            }
+                        }
+                    }
+                }
+            }
+        }
 }
 
 #Preview {
